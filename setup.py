99b879d0f0d27bc8b08f52bc6ffe816a657d7546
joergbuchwald <joerg.buchwald@ufz.de>
2019-12-09 16:57:06 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="vtu2nc4",
+    version=0.01,
+    maintainer="Jörg Buchwald",
+    maintainer_email="joerg_buchwald@ufz.de",
+    author="Jörg Buchwald",
+    author_email="joerg.buchwald@ufz.de",
+    url="https://github.com/joergbuchwald/",
+    license="MIT -  see LICENSE.txt",
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    include_package_data=True,
+    install_requires=[],
+    py_modules=["vtu2nc4"],
+)
